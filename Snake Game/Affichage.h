adcb4fdf9e81ada8c013723ce2bf0faf9e81d56e
Naga <94557604+NagaYZ@users.noreply.github.com>
2021-11-17 18:40:02 +0100

    Delete Snake Game directory

--- Snake Game/Affichage.h
+++
@@ -1,9 +0,0 @@
-#ifndef __AFFICHAGE__
-#define __AFFICHAGE__
-
-void afficher_quadrillage(Monde mon);
-void afficher_pomme(Pomme pom);
-void afficher_serpent(Serpent mon);
-void afficher_monde(Monde pom);
-
-#endif
--- Snake Game/Pomme.h
+++
@@ -1,8 +0,0 @@
-#ifndef __POMME__
-#define __POMME__
-
-typedef struct Case Pomme;
-
-Pomme pomme_gen_alea(int n,int m);
-
-#endif
