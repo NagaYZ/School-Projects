125af72c4aa0f825324ac85234ffacd013fe24b8
Naga <94557604+NagaYZ@users.noreply.github.com>
2021-11-17 18:39:50 +0100

    Boggle Game using ncurses C library

---
+++ Boggle Game/Affichage.h
@@ -0,0 +1,11 @@
+#ifndef __AFFICHAGE__
+#define __AFFICHAGE__
+#include "Grille.h"
+#include "Jeu.h"
+
+void afficher_lettre(Grille grille);
+
+void afficher_mot_en_cours(Mot liste);
+
+void afficher_jeu_fini(int score);
+#endif
---
+++ Boggle Game/Test.h
@@ -0,0 +1,12 @@
+#ifndef __TEST__
+#define __TEST__
+
+int test_ajout_lettre();
+int test_longeur_mot();
+int test_retirer_derniere_lettre();
+int test_recherche();
+int test_ajout_mot_dico();
+int test_mot_valide();
+void test();
+
+#endif
